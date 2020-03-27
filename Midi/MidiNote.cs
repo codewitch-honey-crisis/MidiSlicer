6020e199fdc56ac934a9dbb697300ab8ea425f4d
codewitch honey crisis <gaztoast@gmail.com>
2020-03-26 20:58:52 -0700

    Improved MidiNote to take a note string, and added ToString() override

--- Midi/MidiNote.cs
+++ Midi/MidiNote.cs
@@ -21,6 +21,14 @@
 		/// </summary>
 		public byte NoteId { get; }
 		/// <summary>
+		/// Indicates the MIDI note
+		/// </summary>
+		public string Note { 
+			get {
+				return MidiUtility.NoteIdToNote(NoteId);
+			} 
+		}
+		/// <summary>
 		/// Indicates the velocity (0-127)
 		/// </summary>
 		public byte Velocity { get; }
@@ -43,6 +51,30 @@
 			NoteId = unchecked((byte)(noteId & 0x7F));
 			Velocity= unchecked((byte)(velocity & 0x7F));
 			Length = length;
+		}
+		/// <summary>
+		/// Creates a new MIDI note instance
+		/// </summary>
+		/// <param name="position">The position</param>
+		/// <param name="channel">The channel (0-15)</param>
+		/// <param name="note">The note</param>
+		/// <param name="velocity">The velocity (0-127)</param>
+		/// <param name="length">The length</param>
+		public MidiNote(int position, byte channel, string note, byte velocity, int length)
+		{
+			Position = position;
+			Channel = unchecked((byte)(channel & 0x0F));
+			NoteId = MidiUtility.NoteToNoteId(note);
+			Velocity = unchecked((byte)(velocity & 0x7F));
+			Length = length;
+		}
+		/// <summary>
+		/// Gets a string representation of the note
+		/// </summary>
+		/// <returns>A string representing the note</returns>
+		public override string ToString()
+		{
+			return "Position: " + Position.ToString() + ", Channel: " + Channel.ToString() + ", Note: " + Note + ", Velocity: " + Velocity.ToString() + ", Length: " + Length.ToString();
 		}
 	}
 }
