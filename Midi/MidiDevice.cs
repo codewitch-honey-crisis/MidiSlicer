79469c540a872e3341dc9fb3bb7208bfe974fe4c
codewitch honey crisis <gaztoast@gmail.com>
2020-06-27 21:44:54 -0700

    added MidiInputDevice, MidiMonitor

--- Midi/MidiDevice.cs
+++ Midi/MidiDevice.cs
@@ -17,7 +17,21 @@ namespace M
 		#region Win32
 		[DllImport("winmm.dll")]
 		static extern int midiOutGetNumDevs();
+		[DllImport("winmm.dll")]
+		static extern int midiInGetNumDevs();
 		#endregion
+		/// <summary>
+		/// Indicates the available MIDI input devices
+		/// </summary>
+		public static IList<MidiInputDevice> Inputs {
+			get {
+				var count = midiInGetNumDevs();
+				var result = new List<MidiInputDevice>(count);
+				for (var i = 0; i < count; ++i)
+					result.Add(new MidiInputDevice(i));
+				return result;
+			}
+		}
 		/// <summary>
 		/// Indicates the available MIDI output devices
 		/// </summary>
@@ -77,6 +91,220 @@ namespace M
 		~MidiDevice()
 		{
 			Close();
+		}
+	}
+	/// <summary>
+	/// Represents the arguments to the Input event
+	/// </summary>
+#if MIDILIB
+	public
+#endif
+	class MidiInputEventArgs
+	{
+		/// <summary>
+		/// The timestamp of the message
+		/// </summary>
+		public TimeSpan TimeStamp {get;}
+		/// <summary>
+		/// The message
+		/// </summary>
+		public MidiMessage Message { get; }
+		/// <summary>
+		/// Constructs a new instance
+		/// </summary>
+		/// <param name="timeStamp">The timestamp</param>
+		/// <param name="message">The message</param>
+		public MidiInputEventArgs(TimeSpan timeStamp,MidiMessage message)
+		{
+			TimeStamp = timeStamp;
+			Message = message;
+		}
+	}
+	/// <summary>
+	/// Represents the delegate for the Input event
+	/// </summary>
+	/// <param name="sender">The sender</param>
+	/// <param name="args">The arguments</param>
+#if MIDILIB
+	public
+#endif
+		delegate void MidiInputEventHandler(object sender, MidiInputEventArgs args);
+	/// <summary>
+	/// Indicates the state of the MIDI input device
+	/// </summary>
+#if MIDILIB
+	public
+#endif
+	enum MidiInputDeviceState
+	{
+		/// <summary>
+		/// The device is closed
+		/// </summary>
+		Closed=-1,
+		/// <summary>
+		/// The device has been started
+		/// </summary>
+		Started =0,
+		/// <summary>
+		/// The device is stopped
+		/// </summary>
+		Stopped = 1
+	}
+	/// <summary>
+	/// Represents a MIDI input device
+	/// </summary>
+#if MIDILIB
+	public
+#endif
+	sealed class MidiInputDevice : MidiDevice
+	{
+		#region Win32
+		delegate void MidiInProc(IntPtr handle, int wMsg, int dwInstance, int dwParam1, int dwParam2);
+
+		[DllImport("winmm.dll")]
+		static extern int midiInGetErrorText(int errCode,
+		   StringBuilder message, int sizeOfMessage);
+		[DllImport("winmm.dll")]
+		static extern int midiInGetDevCaps(int deviceIndex,
+			ref MIDIINCAPS caps, int sizeOfMidiInCaps);
+		[DllImport("winmm.dll")]
+		static extern int midiInOpen(out IntPtr refHandle, int uDeviceID, MidiInProc dwCallback, int dwInstance, int dwFlags);
+		[DllImport("winmm.dll")]
+		static extern int midiInClose(IntPtr handle);
+		[DllImport("winmm.dll")]
+		static extern int midiInStart(IntPtr handle);
+		[DllImport("winmm.dll")]
+		static extern int midiInStop(IntPtr handle);
+
+		[StructLayout(LayoutKind.Sequential)]
+		struct MIDIINCAPS
+		{
+			public ushort wMid;
+			public ushort wPid;
+			public uint vDriverVersion;     // MMVERSION
+			[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+			public string szPname;
+			public uint dwSupport;
+		}
+		const int CALLBACK_FUNCTION = 196608;
+		const int MIM_OPEN = 961;
+		const int MIM_CLOSE = 962;
+		const int MIM_DATA = 963;
+		const int MIM_LONGDATA = 964;
+		const int MIM_ERROR = 965;
+		const int MIM_LONGERROR = 966;
+		const int MIM_MOREDATA = 972;
+
+		#endregion
+		readonly MidiInProc _callback;
+		readonly int _index;
+		MIDIINCAPS _caps;
+		IntPtr _handle;
+		MidiInputDeviceState _state;
+		internal MidiInputDevice(int deviceIndex)
+		{
+			if (0 > deviceIndex)
+				throw new ArgumentOutOfRangeException("deviceIndex");
+			_handle = IntPtr.Zero;
+			_callback = new MidiInProc(_MidiInProc);
+			_index = deviceIndex;
+			_state = MidiInputDeviceState.Stopped;
+			_CheckOutResult(midiInGetDevCaps(deviceIndex, ref _caps, Marshal.SizeOf(typeof(MIDIINCAPS))));
+		}
+		/// <summary>
+		/// Raised when incoming messages occur
+		/// </summary>
+		public event MidiInputEventHandler Input;
+		/// <summary>
+		/// Indicates the state of the device
+		/// </summary>
+		public MidiInputDeviceState State => _state;
+		/// <summary>
+		/// Indicates whether the input device is open
+		/// </summary>
+		public override bool IsOpen => IntPtr.Zero != _handle;
+		/// <summary>
+		/// Indicates the index of the input device
+		/// </summary>
+		public override int Index => _index;
+		/// <summary>
+		/// Indicates the name of the input device
+		/// </summary>
+		public override string Name => _caps.szPname;
+		/// <summary>
+		/// Opens the MIDI input device
+		/// </summary>
+		public override void Open()
+		{
+			Close();
+			_CheckOutResult(midiInOpen(out _handle, _index, _callback, 0, CALLBACK_FUNCTION));
+			_state = MidiInputDeviceState.Stopped;
+		}
+		/// <summary>
+		/// Closes the MIDI input device
+		/// </summary>
+		public override void Close()
+		{
+			if(IntPtr.Zero!=_handle)
+			{
+				_CheckOutResult(midiInClose(_handle));
+				_state = MidiInputDeviceState.Closed;
+			}
+		}
+		/// <summary>
+		/// Starts the MIDI input device
+		/// </summary>
+		public void Start()
+		{
+			if (IntPtr.Zero == _handle)
+				throw new InvalidOperationException("The device is closed.");
+			_CheckOutResult(midiInStart(_handle));
+			_state = MidiInputDeviceState.Started;
+		}
+		/// <summary>
+		/// Stops the MIDI input device
+		/// </summary>
+		public void Stop()
+		{
+			if (IntPtr.Zero == _handle)
+				throw new InvalidOperationException("The device is closed.");
+			_CheckOutResult(midiInStop(_handle));
+			_state = MidiInputDeviceState.Stopped;
+		}
+		void _MidiInProc(IntPtr handle, int msg, int instance, int lparam, int wparam)
+		{
+			switch(msg)
+			{
+				case MIM_OPEN:
+					break;
+				case MIM_CLOSE:
+					break;
+				case MIM_DATA:
+					Input?.Invoke(this,new MidiInputEventArgs(new TimeSpan(0, 0, 0, 0, wparam), MidiUtility.UnpackMessage(lparam)));
+					break;
+				case MIM_LONGDATA:
+					break;
+				case MIM_MOREDATA:
+					break;
+				case MIM_ERROR:
+					break;
+				case MIM_LONGERROR:
+					break;
+				default:
+					break;
+			}
+		}
+		static string _GetMidiOutErrorMessage(int errorCode)
+		{
+			var result = new StringBuilder(256);
+			midiInGetErrorText(errorCode, result, result.Capacity);
+			return result.ToString();
+		}
+		[System.Diagnostics.DebuggerNonUserCode()]
+		static void _CheckOutResult(int errorCode)
+		{
+			if (0 != errorCode)
+				throw new Exception(_GetMidiOutErrorMessage(errorCode));
 		}
 	}
 	/// <summary>
---
+++ MidiMonitor/Main.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace MidiMonitor
+{
+	partial class Main
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.InputsComboBox = new System.Windows.Forms.ComboBox();
+			this.InputsLabel = new System.Windows.Forms.Label();
+			this.MessagesTextBox = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// InputsComboBox
+			// 
+			this.InputsComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.InputsComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.InputsComboBox.FormattingEnabled = true;
+			this.InputsComboBox.Location = new System.Drawing.Point(39, 3);
+			this.InputsComboBox.Name = "InputsComboBox";
+			this.InputsComboBox.Size = new System.Drawing.Size(254, 21);
+			this.InputsComboBox.TabIndex = 0;
+			this.InputsComboBox.SelectedIndexChanged += new System.EventHandler(this.InputsComboBox_SelectedIndexChanged);
+			// 
+			// InputsLabel
+			// 
+			this.InputsLabel.AutoSize = true;
+			this.InputsLabel.Location = new System.Drawing.Point(3, 6);
+			this.InputsLabel.Name = "InputsLabel";
+			this.InputsLabel.Size = new System.Drawing.Size(31, 13);
+			this.InputsLabel.TabIndex = 1;
+			this.InputsLabel.Text = "Input";
+			// 
+			// MessagesTextBox
+			// 
+			this.MessagesTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.MessagesTextBox.Location = new System.Drawing.Point(6, 30);
+			this.MessagesTextBox.Multiline = true;
+			this.MessagesTextBox.Name = "MessagesTextBox";
+			this.MessagesTextBox.ReadOnly = true;
+			this.MessagesTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.MessagesTextBox.Size = new System.Drawing.Size(287, 416);
+			this.MessagesTextBox.TabIndex = 2;
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(301, 450);
+			this.Controls.Add(this.MessagesTextBox);
+			this.Controls.Add(this.InputsLabel);
+			this.Controls.Add(this.InputsComboBox);
+			this.Name = "Main";
+			this.Text = "MIDI Monitor";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox InputsComboBox;
+		private System.Windows.Forms.Label InputsLabel;
+		private System.Windows.Forms.TextBox MessagesTextBox;
+	}
+}
+
